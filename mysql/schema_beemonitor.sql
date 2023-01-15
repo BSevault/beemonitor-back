622efd9fd79e88ac1056ebbe6d1abfd3a7f1858e
BSevault <b.sevault@gmail.com>
2023-01-15 16:29:44 +0100

    🗃️ english translation

--- mysql/schema_beemonitor.sql
+++ mysql/schema_beemonitor.sql
@@ -5,29 +5,29 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema ruche_connectee
+-- Schema beemonitor
 -- -----------------------------------------------------
-DROP SCHEMA IF EXISTS `ruche_connectee` ;
+DROP SCHEMA IF EXISTS `beemonitor` ;
 
 -- -----------------------------------------------------
--- Schema ruche_connectee
+-- Schema beemonitor
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `ruche_connectee` DEFAULT CHARACTER SET utf8 ;
-USE `ruche_connectee` ;
+CREATE SCHEMA IF NOT EXISTS `beemonitor` DEFAULT CHARACTER SET utf8 ;
+USE `beemonitor` ;
 
 -- -----------------------------------------------------
--- Table `ruche_connectee`.`utilisateur`
+-- Table `beemonitor`.`person`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `ruche_connectee`.`utilisateur` ;
+DROP TABLE IF EXISTS `beemonitor`.`person` ;
 
-CREATE TABLE IF NOT EXISTS `ruche_connectee`.`utilisateur` (
+CREATE TABLE IF NOT EXISTS `beemonitor`.`person` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `nom` VARCHAR(45) NOT NULL,
-  `prenom` VARCHAR(45) NOT NULL,
+  `first_name` VARCHAR(45) NOT NULL,
+  `last_name` VARCHAR(45) NOT NULL,
   `email` VARCHAR(45) NOT NULL,
   `pwd` VARCHAR(512) NOT NULL,
-  `telephone` VARCHAR(45) NULL,
-  `adresse` VARCHAR(1000) NULL,
+  `phone` VARCHAR(45) NULL,
+  `adress` VARCHAR(1000) NULL,
   `is_admin` TINYINT NULL,
   `is_active` TINYINT NULL,
   PRIMARY KEY (`id`),
@@ -36,148 +36,148 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `ruche_connectee`.`rucher`
+-- Table `beemonitor`.`apiary`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `ruche_connectee`.`rucher` ;
+DROP TABLE IF EXISTS `beemonitor`.`apiary` ;
 
-CREATE TABLE IF NOT EXISTS `ruche_connectee`.`rucher` (
+CREATE TABLE IF NOT EXISTS `beemonitor`.`apiary` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `nom` VARCHAR(45) NULL,
+  `name` VARCHAR(45) NULL,
   `latitude` DECIMAL(6,4) NOT NULL,
   `longitude` DECIMAL(7,4) NOT NULL,
-  `date_creation` DATE NOT NULL,
-  `date_fin_rucher` DATE NULL,
-  `commentaire` TEXT NULL,
+  `creation_date` DATE NOT NULL,
+  `end_date` DATE NULL,
+  `comment` TEXT NULL,
   `is_active` TINYINT NULL,
-  `utilisateur_id` INT UNSIGNED NOT NULL,
+  `person_id` INT UNSIGNED NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_rucher_utilisateur1_idx` (`utilisateur_id` ASC) VISIBLE,
-  CONSTRAINT `fk_rucher_utilisateur1`
-    FOREIGN KEY (`utilisateur_id`)
-    REFERENCES `ruche_connectee`.`utilisateur` (`id`)
+  INDEX `fk_apiary_person1_idx` (`person_id` ASC) VISIBLE,
+  CONSTRAINT `fk_apiary_person1`
+    FOREIGN KEY (`person_id`)
+    REFERENCES `beemonitor`.`person` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `ruche_connectee`.`ruche`
+-- Table `beemonitor`.`hive`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `ruche_connectee`.`ruche` ;
+DROP TABLE IF EXISTS `beemonitor`.`hive` ;
 
-CREATE TABLE IF NOT EXISTS `ruche_connectee`.`ruche` (
+CREATE TABLE IF NOT EXISTS `beemonitor`.`hive` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `nom` VARCHAR(45) NULL,
+  `name` VARCHAR(45) NULL,
   `latitude` DECIMAL(8,6) NOT NULL,
   `longitude` DECIMAL(9,6) NOT NULL,
-  `altitude_en_m` INT NOT NULL,
+  `altitude_in_m` INT NOT NULL,
   `orientation` VARCHAR(45) NOT NULL,
   `type` VARCHAR(45) NOT NULL,
-  `type_abeille` VARCHAR(45) NOT NULL,
-  `poids_initial_en_g` INT NOT NULL,
-  `date_achat` DATE NOT NULL,
-  `nb_hausse` INT NULL,
-  `commentaire` TEXT NULL,
+  `bee_type` VARCHAR(45) NOT NULL,
+  `initial_weight_in_g` INT NOT NULL,
+  `purchase_date` DATE NOT NULL,
+  `super_count` INT NULL,
+  `comment` TEXT NULL,
   `is_active` TINYINT NULL,
   PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `ruche_connectee`.`capteur`
+-- Table `beemonitor`.`sensor`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `ruche_connectee`.`capteur` ;
+DROP TABLE IF EXISTS `beemonitor`.`sensor` ;
 
-CREATE TABLE IF NOT EXISTS `ruche_connectee`.`capteur` (
+CREATE TABLE IF NOT EXISTS `beemonitor`.`sensor` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `ref` VARCHAR(45) NOT NULL,
   `type` VARCHAR(45) NOT NULL,
   `is_active` TINYINT NULL,
-  `id_ruche` INT UNSIGNED NULL,
-  `id_rucher` INT UNSIGNED NULL,
+  `id_hive` INT UNSIGNED NULL,
+  `id_apiary` INT UNSIGNED NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_capteur_ruche1_idx` (`id_ruche` ASC) VISIBLE,
-  INDEX `fk_capteur_rucher1_idx` (`id_rucher` ASC) VISIBLE,
-  CONSTRAINT `fk_capteur_ruche1`
-    FOREIGN KEY (`id_ruche`)
-    REFERENCES `ruche_connectee`.`ruche` (`id`)
+  INDEX `fk_sensor_hive1_idx` (`id_hive` ASC) VISIBLE,
+  INDEX `fk_sensor_apiary1_idx` (`id_apiary` ASC) VISIBLE,
+  CONSTRAINT `fk_sensor_hive1`
+    FOREIGN KEY (`id_hive`)
+    REFERENCES `beemonitor`.`hive` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_capteur_rucher1`
-    FOREIGN KEY (`id_rucher`)
-    REFERENCES `ruche_connectee`.`rucher` (`id`)
+  CONSTRAINT `fk_sensor_apiary1`
+    FOREIGN KEY (`id_apiary`)
+    REFERENCES `beemonitor`.`apiary` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `ruche_connectee`.`mesure`
+-- Table `beemonitor`.`measurement`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `ruche_connectee`.`mesure` ;
+DROP TABLE IF EXISTS `beemonitor`.`measurement` ;
 
-CREATE TABLE IF NOT EXISTS `ruche_connectee`.`mesure` (
+CREATE TABLE IF NOT EXISTS `beemonitor`.`measurement` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `valeur` DECIMAL(7,4) NOT NULL,
-  `date_mesure` DATE NOT NULL,
-  `heure_mesure` TIME NOT NULL,
-  `unite` VARCHAR(45) NOT NULL,
-  `id_capteur` INT UNSIGNED NOT NULL,
+  `value` DECIMAL(7,4) NOT NULL,
+  `measurement_date` DATE NOT NULL,
+  `measurement_hour` TIME NOT NULL,
+  `unit` VARCHAR(45) NOT NULL,
+  `id_sensor` INT UNSIGNED NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_mesure_capteur1_idx` (`id_capteur` ASC) VISIBLE,
-  CONSTRAINT `fk_mesure_capteur1`
-    FOREIGN KEY (`id_capteur`)
-    REFERENCES `ruche_connectee`.`capteur` (`id`)
+  INDEX `fk_measurement_sensor1_idx` (`id_sensor` ASC) VISIBLE,
+  CONSTRAINT `fk_measurement_sensor1`
+    FOREIGN KEY (`id_sensor`)
+    REFERENCES `beemonitor`.`sensor` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `ruche_connectee`.`rucher_has_ruche`
+-- Table `beemonitor`.`apiary_has_hive`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `ruche_connectee`.`rucher_has_ruche` ;
+DROP TABLE IF EXISTS `beemonitor`.`apiary_has_hive` ;
 
-CREATE TABLE IF NOT EXISTS `ruche_connectee`.`rucher_has_ruche` (
-  `id_rucher` INT UNSIGNED NOT NULL,
-  `id_ruche` INT UNSIGNED NOT NULL,
-  `date_mise_en_place` DATE NOT NULL,
-  `date_enlevement` DATE NULL,
-  PRIMARY KEY (`id_rucher`, `id_ruche`),
-  INDEX `fk_rucher_has_ruche_ruche1_idx` (`id_ruche` ASC) VISIBLE,
-  INDEX `fk_rucher_has_ruche_rucher1_idx` (`id_rucher` ASC) VISIBLE,
-  CONSTRAINT `fk_rucher_has_ruche_rucher1`
-    FOREIGN KEY (`id_rucher`)
-    REFERENCES `ruche_connectee`.`rucher` (`id`)
+CREATE TABLE IF NOT EXISTS `beemonitor`.`apiary_has_hive` (
+  `id_apiary` INT UNSIGNED NOT NULL,
+  `id_hive` INT UNSIGNED NOT NULL,
+  `add_date` DATE NOT NULL,
+  `remove_date` DATE NULL,
+  PRIMARY KEY (`id_apiary`, `id_hive`),
+  INDEX `fk_apiary_has_hive_hive1_idx` (`id_hive` ASC) VISIBLE,
+  INDEX `fk_apiary_has_hive_apiary1_idx` (`id_apiary` ASC) VISIBLE,
+  CONSTRAINT `fk_apiary_has_hive_apiary1`
+    FOREIGN KEY (`id_apiary`)
+    REFERENCES `beemonitor`.`apiary` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_rucher_has_ruche_ruche1`
-    FOREIGN KEY (`id_ruche`)
-    REFERENCES `ruche_connectee`.`ruche` (`id`)
+  CONSTRAINT `fk_apiary_has_hive_hive1`
+    FOREIGN KEY (`id_hive`)
+    REFERENCES `beemonitor`.`hive` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `ruche_connectee`.`config_alerte`
+-- Table `beemonitor`.`alert_config`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `ruche_connectee`.`config_alerte` ;
+DROP TABLE IF EXISTS `beemonitor`.`alert_config` ;
 
-CREATE TABLE IF NOT EXISTS `ruche_connectee`.`config_alerte` (
+CREATE TABLE IF NOT EXISTS `beemonitor`.`alert_config` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `type_alerte` VARCHAR(45) NOT NULL,
-  `limite_haute` DECIMAL(5,2) NULL,
-  `limite_basse` DECIMAL(5,2) NULL,
+  `alert_type` VARCHAR(45) NOT NULL,
+  `upper_limit` DECIMAL(5,2) NULL,
+  `lower_limit` DECIMAL(5,2) NULL,
   `variation` DECIMAL(4,2) NULL,
-  `intervalle` INT NULL,
-  `intervalle_unite` VARCHAR(16) NULL,
-  `id_capteur` INT UNSIGNED NOT NULL,
+  `interval` INT NULL,
+  `interval_unit` VARCHAR(16) NULL,
+  `id_sensor` INT UNSIGNED NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_config_alerte_capteur1_idx` (`id_capteur` ASC) VISIBLE,
-  CONSTRAINT `fk_config_alerte_capteur`
-    FOREIGN KEY (`id_capteur`)
-    REFERENCES `ruche_connectee`.`capteur` (`id`)
+  INDEX `fk_alert_config_sensor1_idx` (`id_sensor` ASC) VISIBLE,
+  CONSTRAINT `fk_alert_config_sensor`
+    FOREIGN KEY (`id_sensor`)
+    REFERENCES `beemonitor`.`sensor` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
