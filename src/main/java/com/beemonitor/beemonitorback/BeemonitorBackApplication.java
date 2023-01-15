a3a62b5b0fdfb79511a43600522bf85a852a51a9
BSevault <b.sevault@gmail.com>
2023-01-15 17:50:23 +0100

    Initial commit

---
+++ src/main/java/com/beemonitor/beemonitorback/BeemonitorBackApplication.java
@@ -0,0 +1,13 @@
+package com.beemonitor.beemonitorback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeemonitorBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeemonitorBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/beemonitor/beemonitorback/BeemonitorBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beemonitor.beemonitorback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeemonitorBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
