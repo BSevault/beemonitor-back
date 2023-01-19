27688de12c6e8ee50b076d1ba18635fa94faf01c
BSevault <b.sevault@gmail.com>
2023-01-19 23:02:50 +0100

    nothing

--- src/main/java/com/beemonitor/beemonitorback/repository/ApiaryRepository.java
+++ src/main/java/com/beemonitor/beemonitorback/repository/ApiaryRepository.java
@@ -4,5 +4,7 @@ import org.springframework.stereotype.Repository;
 import com.beemonitor.beemonitorback.model.Apiary;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 @Repository
 public interface ApiaryRepository extends CrudRepository<Apiary, Long> {}
