ad27a1a21225f309162acdd59fa950942020dd99
BSevault <b.sevault@gmail.com>
2023-04-28 09:50:21 +0200

    typo

--- src/main/java/com/beemonitor/beemonitorback/dto/handler/AbstractDtoHandler.java
+++ src/main/java/com/beemonitor/beemonitorback/dto/handler/AbstractDtoHandler.java
@@ -3,9 +3,8 @@ package com.beemonitor.beemonitorback.dto.handler;
 abstract class AbstractDtoHandler {
 
     protected static String checkAndClean(String pValue) {
-        if (pValue == null || pValue.trim().isEmpty()) {
+        if (pValue == null || pValue.trim().isEmpty())
             return null;
-        }
         return pValue.trim();
     }
 
