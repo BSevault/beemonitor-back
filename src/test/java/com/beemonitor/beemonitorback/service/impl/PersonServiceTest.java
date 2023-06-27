14502075e396027ccb64dee53980255a31641c00
BSevault <b.sevault@gmail.com>
2023-06-27 11:56:50 +0200

    add test

---
+++ src/test/java/com/beemonitor/beemonitorback/service/impl/PersonServiceTest.java
@@ -0,0 +1,51 @@
+package com.beemonitor.beemonitorback.service.impl;
+
+import com.beemonitor.beemonitorback.dto.in.PersonDtoIn;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+@Rollback(value = false)
+class PersonServiceTest {
+
+    @Autowired
+    private PersonService personService;
+
+    @Test
+    @Rollback()
+    void insertValidUser() {
+
+        PersonDtoIn dto = new PersonDtoIn();
+        dto.setFirstName("Bob");
+        dto.setLastName("Marley");
+        dto.setEmail("bob2@gmail.com");
+        dto.setPwd("test");
+
+        var entity = personService.insert(dto);
+
+        Assertions.assertNotNull(entity.getId(), "Id de la nouvelle entité est null");
+
+        Assertions.assertEquals(dto.getEmail(), entity.getEmail(), "Le mail créé n'est pas celui envoyé");
+    }
+
+    @Test
+    @Rollback()
+    void insertExistingEmail() {
+
+        PersonDtoIn dto = new PersonDtoIn();
+        dto.setFirstName("Bob");
+        dto.setLastName("Marley");
+        dto.setEmail("bob@gmail.com");
+        dto.setPwd("test");
+
+        var entity = personService.insert(dto);
+
+        Assertions.assertNull(entity, "Id de la nouvelle entité est null");
+    }
+
+}
