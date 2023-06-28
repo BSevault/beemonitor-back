a9e0af7e835ae38f626bdde3232f6bb55a96d617
BSevault <b.sevault@gmail.com>
2023-06-28 15:46:10 +0200

    typo

--- src/main/java/com/beemonitor/beemonitorback/controller/PersonController.java
+++ src/main/java/com/beemonitor/beemonitorback/controller/PersonController.java
@@ -30,8 +30,6 @@ public class PersonController {
 
     private final PersonService personService;
 
-
-
     @Autowired
     public PersonController(PersonService personService) {
         this.personService = personService;
