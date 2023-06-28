6d5b0bdc54507d639de85efc2300ca76f003c150
BSevault <b.sevault@gmail.com>
2023-06-28 15:45:37 +0200

    change to jpa repo

--- src/main/java/com/beemonitor/beemonitorback/repository/ApiaryRepository.java
+++ src/main/java/com/beemonitor/beemonitorback/repository/ApiaryRepository.java
@@ -1,8 +1,8 @@
 package com.beemonitor.beemonitorback.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.beemonitor.beemonitorback.model.ApiaryEntity;
-import org.springframework.data.repository.CrudRepository;
 
 @Repository
-public interface ApiaryRepository extends CrudRepository<ApiaryEntity, Long> {}
+public interface ApiaryRepository extends JpaRepository<ApiaryEntity, Long> {}
--- src/main/java/com/beemonitor/beemonitorback/repository/HiveRepository.java
+++ src/main/java/com/beemonitor/beemonitorback/repository/HiveRepository.java
@@ -1,6 +1,9 @@
 package com.beemonitor.beemonitorback.repository;
 
 import com.beemonitor.beemonitorback.model.Hive;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
-public interface HiveRepository extends CrudRepository<Hive, Long> {}
+@Repository
+public interface HiveRepository extends JpaRepository<Hive, Long> {}
